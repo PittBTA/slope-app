5b17f34f8882a6a68d31dea02580ac9bfefdb741
matthewmaribojoc <matthewmaribojoc@gmail.com>
2019-02-11 20:47:10 -0500

    added map.js

---
+++ src/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import GoogleMapReact from 'google-map-react';
+
+class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: 40.4444,
+      lng: -79.9608
+    },
+    zoom: 16
+  };
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyC6A9F8FIGDy-YOWv0hLQyW33wfltkG40Q' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default App;
